e57c9bcab9f2648cc22ed433d404089883a22ae4
Vebnik(Nik) <vebnik111@gmail.com>
2022-11-25 14:03:26 +0300

    add test for LIST_PRODUCTS

---
+++ tests/test_get_product_list.py
@@ -0,0 +1,16 @@
+from hanlders import products
+
+
+def data():
+  return   {
+    "action": 2,
+    "filter": {
+      "price": ">= 100",
+      "category": 1
+    }
+  }
+
+
+def test_get_product_list(data = data()):
+  true_dayta = {'code': 200, 'data': '1. Яблоки. Голден. (1000 руб/кг)\n3. Черешня. Хелл. (100 руб/кг)'}
+  assert products.get_product_list(data) == true_dayta
